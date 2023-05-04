cb848967944c572342226505fddd3e846e78cca8
halvaradop_ <hernanvid123@gmail.com>
2023-05-04 14:30:40 -0500

    feature in main: form and commentaries

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,11 @@
-import heroMobile from "../assets/illustration-1.svg"
-import heroDesktop from "../assets/curve-desktop.svg"
+import illustrationOne from "../assets/illustration-1.svg"
+import illustrationTwo from "../assets/illustration-2.svg"
+import arrow from "../assets/arrow.svg"
+import quotes from "../assets/quotes.svg"
+import user from "../assets/testimonial.jpg"
 import { Input } from "./Input"
+import { Form } from "./Form"
+import { Picture } from "./Picture"
 import { useState } from "react"
 
 const Main = () => {
@@ -10,23 +15,63 @@ const Main = () => {
     const handleEmail = (event) => setEmail(event.target.value)
 
     return (
-        <main className="w-90 mt-5 mx-auto">
-            <picture>
-                <source media="(min-width: )" srcSet={heroDesktop} />
-                <img className="w-full" src={heroMobile} alt="hero image" />
-            </picture>
-            <article className="mb-56 flex flex-col gap-y-3 text-center">
-                <h1 className="text-blue-300 text-lg font-bold font-open">All your files in one secure location, accessible anywhere.</h1>
-                <p className="text-blue-300 text-sm font-medium">
-                    Fylo stores your most important files in one secure location. Access them wherever 
-                    you need, share and collaborate with friends, family, and co-workers.
+        <>
+            <main className="w-90 mt-5 mx-auto">
+                <section>
+                    <img className="w-full" src={illustrationOne} alt="hero image" />
+                    <article className="mb-56 flex flex-col gap-y-3 text-center">
+                        <h1 className="text-blue-300 text-lg font-bold font-open">All your files in one secure location, accessible anywhere.</h1>
+                        <p className="text-blue-300 text-sm font-medium">
+                            Fylo stores your most important files in one secure location. Access them wherever
+                            you need, share and collaborate with friends, family, and co-workers.
+                        </p>
+                        <Form value="Get Started" />
+                    </article>
+                </section>
+                <section>
+                    <img className="w-full" src={illustrationTwo} alt="people picture" />
+                    <h2 className="mt-8 mb-3 text-blue-300 text-md font-bold text-center">Stay productive, wherever you are</h2>
+                    <div className="flex flex-col gap-y-2.5 text-sm text-blue">
+                        <p>
+                            Never let location be an issue when accessing your files. Fylo has you
+                            covered for all of your file storage needs.
+                        </p>
+                        <p>
+                            Securely share files and folders with friends, family and colleagues for
+                            live collaboration. No email attachments required!
+                        </p>
+                    </div>
+                    <a className="mt-6 mb-8 flex items-center justify-center gap-x-1 text-sm text-cyan underline underline-offset-8 decoration-cyan" href="#">
+                        See how Fylo works
+                        <img src={arrow} alt="arrow icon" />
+                    </a>
+                    <article className="w-3/4 mx-auto mb-10">
+                        <figure>
+                            <img src={quotes} alt="quote icon" />
+                            <figcaption className="mt-0.5 mb-2 text-xs text-blue-300 leading-relaxed text-justify">
+                                Fylo has improved our team productivity by an order of magnitude. Since
+                                making the switch our team has become a well-oiled collaboration machine.
+                            </figcaption>
+                        </figure>
+                        <figure className="flex items-center gap-x-1">
+                            <img className="w-3.5 rounded-full" src={user} alt="user picture" />
+                            <figcaption>
+                                <h3 className="text-blue-300 font-bold text-xs">Kyle Burton</h3>
+                                <p className="text-blue text-[1rem]">Founder & CEO, Huddle</p>
+                            </figcaption>
+                        </figure>
+                    </article>
+                </section>
+            </main>
+            <section className="w-full py-5 px-3 flex items-center justify-center flex-col text-white text-center bg-blue">
+                <h2 className="text-base font-bold">Get early access today</h2>
+                <p className="mt-1.5 mb-3 text-sm">
+                    It only takes a minute to sign up and our free starter tier is extremely generous. 
+                    If you have any questions, our support team would be happy to help you.
                 </p>
-                <form className="flex flex-col gap-y-2">
-                    <Input type="email" color="gray" value={email} placeholder="Enter your email" handleChange={handleEmail} />
-                    <Input type="submit" color="blue" value="Get Started" />
-                </form>
-            </article>
-        </main>
+                <Form value="Get Started For Free" />
+            </section>
+        </>
     )
 }
 
