e9b7ef43a3a08126d3bace84f0059aa2dc2e3e87
halvaradop_ <hernanvid123@gmail.com>
2023-05-04 15:02:48 -0500

    main section for tablet devices

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,7 +8,7 @@ const Form = ({ value }) => {
     const handleSubmit = (event) => event.preventDefault();
 
     return (
-        <form className="w-full flex flex-col gap-y-1">
+        <form className="w-full max-w-3xl mx-auto flex flex-col gap-y-1">
             <Input type="email" color="gray" value={email} placeholder="Enter your email" handleChange={handleEmail} />
             <Input type="submit" color="blue" value={value} handleClick={handleSubmit} />
         </form>
