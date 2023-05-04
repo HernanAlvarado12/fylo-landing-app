f017a280de89c4f9754060af9cfb6457006886da
halvaradop_ <hernanvid123@gmail.com>
2023-05-04 14:49:55 -0500

    footer section: list items for contact us

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,15 @@
 import { ListItem } from "./ListItem"
+import { List } from "./List"
 import logo from "../assets/logo.svg"
+
+const items = ['Features', 'Team', 'Sign In']
 
 const Header = () => {
     return (
         <header>
             <nav className="w-90 h-8 mx-auto flex items-center justify-between">
                 <img className="h-3" src={logo} alt="logo icon" />
-                <ul className="flex items-center gap-x-2">
-                    <ListItem text="Features" color="text-blue-300" />
-                    <ListItem text="Team" color="text-blue-300" />
-                    <ListItem text="Sign In" color="text-blue-300" />
-                </ul>
+                <List classList="flex items-center gap-x-2" items={items} color="text-blue-300" />
             </nav>
         </header>
     )
