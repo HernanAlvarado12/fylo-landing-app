c04dadf0234798c779ca9fe58629257b10947697
halvaradop_ <hernanvid123@gmail.com>
2023-05-04 19:22:17 -0500

    footer section for devices desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ const contact = ['Phone: +1-543-123-4567', 'example@fylo.com']
 const Footer = () => {
     return (
         <footer className="w-full bg-blue-300">
-            <section className="w-90 mx-auto py-6 px-2 flex items-start flex-col gap-y-4">
-                <List classList="flex flex-col gap-y-1 gap-x-2" items={contact} color="text-white" />
-                <List classList="flex flex-col gap-y-1 gap-x-2" items={about} color="text-white" />
-                <List classList="flex flex-col gap-y-1 gap-x-2" items={terms} color="text-white" />
+            <section className="w-90 mx-auto py-6 px-2 flex items-start flex-col gap-y-4 md:flex-row">
+                <List classList="flex flex-col gap-y-1 gap-x-2 md:w-1/3 md:pl-4" items={contact} color="text-white" />
+                <List classList="flex flex-col gap-y-1 gap-x-2 md:w-1/3 md:pl-4" items={about} color="text-white" />
+                <List classList="flex flex-col gap-y-1 gap-x-2 md:w-1/3 md:pl-4" items={terms} color="text-white" />
             </section>
         </footer>
     )
